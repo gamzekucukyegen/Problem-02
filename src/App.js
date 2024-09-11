4efaf007590ee3600ce0497d52011baf7b6413cc
gamzekucukyegen <you@example.com>
2024-09-11 12:04:38 +0300

    Problem -02

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Discussion from "./discussion"
+
+
+export default function Discussions() {
+  return (
+    <ul className='discussions'>
+      <Discussion />
+      <Discussion />
+    </ul>
+  )
+}
+
+
